625154665efa6374ce2973dcd5fbf29e597762cc
CristhianBS3008 <cristhianbersa@gmail.com>
2018-03-18 19:37:57 -0500

    Agregando Personaje usando Sprites

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import pygame
+import player
 
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
@@ -21,12 +23,13 @@ dimensiones = (500,500)
 pantalla = pygame.display.set_mode(dimensiones)
 pygame.display.set_caption("CRASH BASH")
 reloj = pygame.time.Clock()
+velocidad = 7
+personaje = player.Kate((0, 0), 7)
 
 
 while not hecho:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            print("El usuario solicito salir.")
             hecho = True
 
     pantalla.fill(NEGRO)
@@ -36,7 +39,10 @@ while not hecho:
             color = BLANCO
             pygame.draw.rect(pantalla, color,[(MARGEN+LARGO) * columna + MARGEN,(MARGEN+ALTO) * fila + MARGEN,LARGO,ALTO] )
 
+    personaje.handle_event(evento)
+    pantalla.blit(personaje.image, personaje.rect)
+
     pygame.display.flip()
-    reloj.tick(60)
+    reloj.tick(40)
 
 pygame.quit()
